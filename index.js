bd1215022ce6d391d7dd275306179b1b30865ae9
santoshkrmanjhi <141865683+santoshkrmanjhi@users.noreply.github.com>
2024-05-11 13:12:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV ==="production"){
   const dirPath = path.resolve();
   app.use(express.static("Frontend/dist"));
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(dirPath,"Frontend","dist","index.html"))
+    res.sendFile(path.resolve("Frontend","dist","index.html"))
   })
 }
 
